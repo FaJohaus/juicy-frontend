5971386c481bd3ff9c29d3d4a267ca764c90efee
Fabian5150 <fabian.strauch.04@web.de>
2024-11-27 02:32:53 +0100

    Upper Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { Flex, Spacer, Avatar, Button, Center, IconButton } from "@chakra-ui/react";
-import { ChevronDownIcon, SearchIcon, BellIcon } from "@chakra-ui/icons";
+import { Flex, Spacer, Avatar, Button, Center, IconButton, Text } from "@chakra-ui/react";
+import { ChevronDownIcon, SearchIcon, BellIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import pageTitles from '../assets/pageTitles.json'
@@ -15,27 +15,29 @@ const Navbar = ({ children }) => {
 
     return (
         <>
-            <Flex h='55px'>
+            <Flex h='55px' borderWidth='1px'>
                 {/* LEFT SIDE */}
-                {pageTitle}
+                <Center>
+                    <IconButton ml={2} icon={<HamburgerIcon boxSize={5} />} variant='ghost' rounded={100} />
+                    <Text fontSize='xl' ml={2} pt={1}>
+                        {pageTitle}
+                    </Text>
+                </Center>
                 <Spacer />
                 {/* RIGHT SIDE */}
                 <Center>
-                    <IconButton icon={<BellIcon boxSize={6} />} variant='ghost' />
-                </Center>
-                <Center>
-                    <IconButton icon={<SearchIcon />} variant='ghost' />
-                </Center>
-                <Center>
+                    <IconButton icon={<BellIcon boxSize={5} />} variant='ghost' rounded={100} />
+                    <IconButton icon={<SearchIcon boxSize={4} />} variant='ghost' rounded={100} />
                     <Button
                         rightIcon={<ChevronDownIcon />}
                         variant='ghost'
                         onClick={() => alert("Gotcha!")}
                         w={20}
+                        rounded={100}
                     >
                         <Avatar
                             src="https://i.pinimg.com/736x/ca/78/3f/ca783fbe94d559e23dc9b7dcc4065a42.jpg"
-                            boxSize={8}
+                            boxSize={7}
                         />
                     </Button>
                 </Center>
