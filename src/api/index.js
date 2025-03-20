698adddb8f366f50962dfb9e16c211eef0045e0d
Fabian5150 <fabian.strauch.04@web.de>
2025-03-20 23:20:40 +0100

    dfgiughf

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/api'
+    withCredentials: true,
+    baseURL: 'http://localhost:3000/api',
+    headers: {
+        "Access-Control-Allow-Origin": "localhost:3000"
+    }
 });
